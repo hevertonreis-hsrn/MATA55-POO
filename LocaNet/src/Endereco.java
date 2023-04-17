b23c8999c8adbcc63398c257c29b6f22cc74b87e
Heverton Reis <heverton.reis04@gmail.com>
2023-04-17 14:49:21 -0300

    doc: Exercício Incremental 03

---
+++ LocaNet/src/Endereco.java
@@ -0,0 +1,63 @@
+public class Endereco {
+    
+    private String rua;
+    private String numero;    
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public Endereco(String rua, String numero, String cidade, String estado, String cep) {
+        
+        this.rua = rua;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+
+    }
+
+    public String getRua() {
+        return this.rua;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    public String getCep() {
+        return this.cep;
+    }
+
+    public String getEstado() {
+        return this.estado;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void atualizaEndereco(String rua,String numero,String cep,String estado,String cidade){
+        this.rua = rua;
+        this.numero = numero;
+        this.cep = cep;
+        this.estado = estado;
+        this.cidade = cidade;
+    }
+
+    public void atualizaEndereco(String rua,String numero,String cep){
+        this.rua = rua;
+        this.numero = numero;
+        this.cep = cep;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Endereço:\n\n" +
+                "Logradouro: " + this.rua + "\n" +
+                "Número: " + this.numero + "\n" +
+                "CEP: " + this.cep + "\n" +
+                "Estado: " + this.estado + "\n" +
+                "Cidade: " + this.cidade + "\n\n";
+    }
+}
--- LocaNet/src/Principal.java
+++ LocaNet/src/Principal.java
@@ -1,28 +1,44 @@
 public class Principal {
     
     public static void main(String[] args) {
+
+        Imovel aptoCampo;
+        Proprietario propAptoCampo;
+        Imovel aptoCidade;
+
+        String estado = "";
+        String uf = "JC";
+        Boolean estadoValido = false;
+
+        for (Estados estados : Estados.values()) {
+            if(uf == estados.getUF()){
+                estadoValido = true;
+                estado = estados.getUF();
+            }
+        }
         
-        Imovel aptoCampo = new Imovel(
+        if(estadoValido == true){
+            aptoCampo = new Imovel(
             "000000123456",
             "Rua Augusta Brasil",
             "123A",
             "01234-567" , 
-            "Bahia", 
+            estado, 
             "Alagoinhas", 
             "apto", 
             "campo");
 
-        Proprietario propAptoCampo = new Proprietario(
+            propAptoCampo = new Proprietario(
             "Urias Manoel de Souza", 
             "012.345.678-90", 
             "0123456789 SSP-BA", 
             "Rua Augusta Brasil",
             "123A",
             "01234-567" , 
-            "Bahia", 
+            estado, 
             "Alagoinhas");
 
-        Imovel aptoCidade = new Imovel(
+            aptoCidade = new Imovel(
             "000000123499",
             "Rua 13 de Maio",
             "0001",
@@ -30,55 +46,47 @@ public class Principal {
             "apto", 
             "praia");
 
-        System.out.println(
+            System.out.println(
             "O senhor(a) " + propAptoCampo.getNome() +
             " é proprietário(a) do imóvel de IPTU: " + aptoCampo.getNumIPTU() + "\n"
             );
 
-        System.out.println(
-            "O imóvel de IPTU: " + aptoCampo.getNumIPTU() +
-            " fica localizado no seguinte endereço:\n\n" +
-            "Logradouro: " + aptoCampo.getRua() + "\n" +
-            "Número: " + aptoCampo.getNumero() + "\n" +
-            "CEP: " + aptoCampo.getCep() + "\n" +
-            "Estado: " + aptoCampo.getEstado() + "\n" +
-            "Cidade: " + aptoCampo.getCidade() + "\n\n"
-        );
-
-        System.out.println(
-            "O senhor(a) " + propAptoCampo.getNome() +
-            " deseja alugar o imóvel de IPTU: " + aptoCidade.getNumIPTU() + "\n"
+            System.out.println(
+                "O imóvel de IPTU: " + aptoCampo.getNumIPTU() + 
+                " fica localizado no seguinte endereço:\n\n" +
+                aptoCampo.getEndereco()            
             );
 
-        System.out.println(
-            "O imóvel de IPTU: " + aptoCidade.getNumIPTU() +
-            " fica localizado no seguinte endereço:\n\n" +
-            "Logradouro: " + aptoCidade.getRua() + "\n" +
-            "Número: " + aptoCidade.getNumero() + "\n" +
-            "CEP: " + aptoCidade.getCep() + "\n" +
-            "Estado: " + aptoCidade.getEstado() + "\n" +
-            "Cidade: " + aptoCidade.getCidade() + "\n\n"
-        );
+            System.out.println(
+                "O senhor(a) " + propAptoCampo.getNome() + 
+                " deseja alugar o imóvel de IPTU: " + aptoCidade.getNumIPTU() + "\n"
+                );
 
-        System.out.println(
-            "O senhor(a) " + propAptoCampo.getNome() +
-            " alugou o imóvel de IPTU: " + aptoCidade.getNumIPTU() + "\n" +
-            "Seu novo Endereço é: \n"
-        );
+            System.out.println(
+                "O imóvel de IPTU: " + aptoCidade.getNumIPTU() +
+                " fica localizado no seguinte endereço:\n\n" +
+                aptoCidade.getEndereco()
+            );
 
-        propAptoCampo.atualizaEndereco(
-            "Rua 13 de Maio",
-            "0001",
-            "01234-775",
-            "Bahia",
-            "Salvador");
+            System.out.println(
+                "O senhor(a) " + propAptoCampo.getNome() +
+                " alugou o imóvel de IPTU: " + aptoCidade.getNumIPTU() + "\n" +
+                "Seu novo Endereço é: \n"
+            );
 
-        System.out.println(
-            "Logradouro: " + propAptoCampo.getRua() + "\n" +
-            "Número: " + propAptoCampo.getNumero() + "\n" +
-            "CEP: " + propAptoCampo.getCep() + "\n" +
-            "Estado: " + propAptoCampo.getEstado() + "\n" +
-            "Cidade: " + propAptoCampo.getCidade() + "\n\n"
-        );
+            propAptoCampo.atualizaEndereco(
+                "Rua 13 de Maio",
+                "0001",
+                "01234-775",
+                Estados.BA.getUF(),
+                "Salvador");
+
+            System.out.println(
+                propAptoCampo.getEndereco()
+            );
+        } else {
+            System.out.println("A UF informada é inválida!!!");
+        }
+
     }
 }
